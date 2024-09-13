815bd568805de05d41adcc2abf0f1b23f4213167
raphaelgrahl1101 <raphael.grahl@otto.de>
2024-09-13 15:39:47 +0200

    Basic Settings

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 import pygame
+from datetime import datetime
 
 class GUI:
 
     coins = 0
     employees = 0
     rating = 0
+    buildings = []
+    time = datetime.now()
+    level = 0
 
     def __init__(self):

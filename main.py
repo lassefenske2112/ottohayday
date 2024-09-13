4b3babe4041f0c180ffb47c8472d66a9e02960b0
raphaelgrahl1101 <raphael.grahl@otto.de>
2024-09-13 15:19:11 +0200

    Basic Settings

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pygame
+
+#game setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60) #FPS
+
+pygame.quit()

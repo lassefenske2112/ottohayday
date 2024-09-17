85704854347a33fabd9a8052ac248a7b5471b37b
LAFENSKE <lassemads.fenske@otto.de>
2024-09-17 10:26:29 +0200

    dectects button press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import MOUSEBUTTONDOWN
+
 from gui import *
 import sys
 
@@ -9,16 +11,20 @@ pygame.display.set_caption("OTTOmatisch")
 clock = pygame.time.Clock()
 running = True
 
-
+testButton = Button(125, 125, 150, 150)
+screen.fill("purple")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+        if event.type == MOUSEBUTTONDOWN:
+            if testButton.button_rect.collidepoint(event.pos):
+                screen.fill("white")
 
-    testButton = Button(125, 125, 150, 150)
+    # fill the screen with a color to wipe away anything from last frame
+
+
+
     screen.blit(testButton.buttonSurface, (testButton.button_rect.x, testButton.button_rect.y))
     # flip() the display to put your work on screen
     #pygame.draw.rect(screen, "black", pygame.Rect(400, 400, 150, 150), 2, 10)

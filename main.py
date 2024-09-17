3436b219c3dedd6c98a1676c78901deae0259f1c
LAFENSKE <lassemads.fenske@otto.de>
2024-09-17 10:14:42 +0200

    displays black rectangle(button prototype)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from gui import *
 import sys
 
 #game setup
@@ -8,20 +9,19 @@ pygame.display.set_caption("OTTOmatisch")
 clock = pygame.time.Clock()
 running = True
 
-button = pygame.Surface((150, 150))
-button_rect = pygame.Rect(125, 125, 150, 50)
-
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    screen.blit(button, (button_rect.x, button_rect.y))
+    testButton = Button(125, 125, 150, 150)
+    screen.blit(testButton.buttonSurface, (testButton.button_rect.x, testButton.button_rect.y))
     # flip() the display to put your work on screen
+    #pygame.draw.rect(screen, "black", pygame.Rect(400, 400, 150, 150), 2, 10)
     pygame.display.flip()
 
     clock.tick(30) #FPS
